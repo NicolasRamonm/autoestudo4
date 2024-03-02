e65fe8b4ce361ab7356efa5a8b897d620dabb4da
NicolasRamonm <nicolas.silva@sou.inteli.edu.br>
2024-03-02 11:19:29 -0300

    Update final.js

--- fases/final.js
+++ fases/final.js
@@ -10,6 +10,7 @@ class Telafinal extends Phaser.Scene {
     create(){
         this.add.image(400, 1100, 'fundodeserto');//background
         this.add.image(400, 980, 'restart').setScale(0.2)//mensagem de fim
+        this.cameras.main.fadeIn(4000, 49, 46, 43);//efeito de fadeIn
 
     }
 
